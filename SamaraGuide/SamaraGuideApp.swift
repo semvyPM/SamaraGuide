fc611840d909593e38a3af1707045e412e0339a7
semvy <semvy@inbox.ru>
2024-04-05 15:24:56 +0400

    Initial Commit

---
+++ SamaraGuide/SamaraGuideApp.swift
@@ -0,0 +1,32 @@
+//
+//  SamaraGuideApp.swift
+//  SamaraGuide
+//
+//  Created by Victoria Semenycheva on 05.04.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SamaraGuideApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
